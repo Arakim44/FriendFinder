bae00386cd2c47b5bc6b9c2e5f8dfd8a7808ee0f
Ara Kim <ahrakim44@gmail.com>
2017-01-26 09:56:55 -0600

    appRoutes comparing the score done, but not storing the data yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,7 +16,59 @@ module.exports = function(app){
   //In each of the below cases, when a use submits form data( a JSON object)
   // ...the JSON is pushed to the appropriate javascript array
   app.post("/api/friends", function(req,res){
-    
+    // console.log(req.body.scores);
+    var newFriend = req.body;
+    var newFriendScore =[];
+
+
+   //change score of strings into numbers
+    for(var i = 0; i < newFriend.scores.length;i++){
+      newFriendScore[i] = parseInt(newFriend.scores[i]);
+    }
+
+    // console.log(newFriendScore);
+    var differenceArr = [];
+    var minIndex = 0;
+    //looping through all the friends that has saved in friends.js
+    for (var i = 0; i <friendsData.length; i++){
+      var comparingFriend = friendsData[i];
+      var totalDiff = 0;
+      var totalDiffArr = [];
+
+    //looping one selected friend from friend.js and looping trough his or her scores.
+      for(var j = 0; j < comparingFriend.scores.length; j++){
+       var eachDiffScore = Math.abs((parseInt(newFriendScore[j]) - parseInt(comparingFriend.scores[j])));
+       totalDiff += eachDiffScore;
+
+      }
+
+      totalDiffArr.push(parseInt(totalDiff));
+      //comparing score through array of total diff.
+      if(i > 0){
+        if(totalDiffArr[minIndex]>totalDiffArr[i]){
+          minIndex = i;
+        }
+      }
+
+
+    }
+    //pick a friends who has the minimum score difference, and send response to display who.
+    res.json(friendsData[minIndex]);
+    //storing to friend.js the new added friend.
+    friendsData.push(newFriend);
+
+
   })
+
+  //This code can ceear out the friendsData while working with the functionallity.
+  // app.post('/api/clear', function(req, res) {
+  //     // Empty out the arrays of data
+  //     friendsData = [];
+  //
+  //
+  //     console.log(friendsData);
+  // });
+
+
 
 }
